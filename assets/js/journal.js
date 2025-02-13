270c0893f2a856d7d321852dd2d7b696a3e345da
scie <emabicova@seznam.cz>
2025-02-13 09:53:06 +0100

    setup

---
+++ assets/js/journal.js
@@ -0,0 +1,11 @@
+// fetch favourited pokémon from local storage and populate the page into cards
+    //cards styling, copied presumably
+
+
+//function to remove favourited pokémon
+
+
+//add notes fucntion
+
+
+//remove notes function
